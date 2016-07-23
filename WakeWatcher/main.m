77393024f4750929a844af9c7a0f8fcd7ec4da65
Eric Kobrin <erluko@gmail.com>
2016-07-23 08:33:17 -0400

    Initial Commit

---
+++ WakeWatcher/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WakeWatcher
+//
+//  Created by Eric Kobrin on 7/23/16.
+//  Copyright (c) 2016 Eric Kobrin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
