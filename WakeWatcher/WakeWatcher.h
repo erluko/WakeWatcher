dc29b05664f88f8e3709fbf9974a6e62cc61cf03
Eric Kobrin <erluko@gmail.com>
2016-07-24 07:15:33 -0400

    Code actually works

--- WakeWatcher/WakeWatcher.h
+++ WakeWatcher/WakeWatcher.h
@@ -9,6 +9,9 @@
 #import <Foundation/Foundation.h>
 
 @interface WakeWatcher : NSObject
+@property (nonatomic, retain) NSString *path;
+@property (nonatomic, retain) NSString *executable;
+
 
 - (void) registerNotifications;
 
--- WakeWatcher/WakeWatcher.m
+++ WakeWatcher/WakeWatcher.m
@@ -12,6 +12,7 @@
 @import AppKit;
 
 
+
 - (void) receiveSleepNote: (NSNotification*) note
 {
     NSLog(@"receiveSleepNote: %@", [note name]);
@@ -20,10 +21,28 @@
 - (void) receiveWakeNote: (NSNotification*) note
 {
     NSLog(@"receiveWakeNote: %@", [note name]);
+    if([[NSFileManager defaultManager] isExecutableFileAtPath:self.path]){
+        NSLog(@"Attempting to execute: '%@'", self.path);
+        NSTask *task;
+        @try {
+            task = [NSTask launchedTaskWithLaunchPath:self.path arguments:[NSArray arrayWithObject:self.executable]];
+        }
+        @catch (NSException *exception) {
+            NSLog(@"Error executing: '%@': %@", self.path, exception.reason);
+        }
+    } else {
+        NSLog(@"No executable file at: '%@'", self.path);
+    }
 }
 
 - (void) registerNotifications
 {
+    if(self.executable == nil){
+        self.executable = @".onwake";
+    }
+    if(self.path == nil){
+        self.path = [NSHomeDirectory() stringByAppendingPathComponent:self.executable];
+    }
     //These notifications are filed on NSWorkspace's notification center, not the default
     // notification center. You will not receive sleep/wake notifications if you file
     //with the default notification center.
