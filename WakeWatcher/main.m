4a099d46373c45597448311fd13dfd484cf2509b
johnmikep <45859899+johnmikep@users.noreply.github.com>
2020-05-04 13:30:19 -0500

    Update main.m

--- WakeWatcher/main.m
+++ WakeWatcher/main.m
@@ -1,21 +1,22 @@
 //
-//  main.m
+//  WakeWatcher.m
 //  WakeWatcher
 //
 //  Created by Eric Kobrin on 7/23/16.
+//  Updated by John Peterson on 5/1/20
 //  Released under CC0 1.0 (Public Domain)
 //    https://creativecommons.org/publicdomain/zero/1.0/
-//
+
+
 
 #import <Foundation/Foundation.h>
-#import "WakeWatcher.h"
+#import "wakeWatcher.h"
 
 int main(int argc, const char * argv[]) {
-    // TODO: run under launchd
     @autoreleasepool {
         NSLog(@"Starting Wake Watcher!");
         @try {
-            id watcher = [[WakeWatcher alloc] init];
+            id watcher = [[wakeWatcher alloc] init];
             NSLog(@"Watcher %@", watcher);
             [watcher registerNotifications];
             [[NSRunLoop currentRunLoop] run];
